caa46f1fd7066738d257cd842ae8bee761b2580b
Asmin Shrestha <shrestha.asmin17@gmail.com>
2021-11-11 18:48:43 +0545

    updated heroku password db

--- main/main.go
+++ main/main.go
@@ -10,10 +10,7 @@ func main() {
 	mappings.CreateUrlMappings()
 
 	// Listen and server on 0.0.0.0:8080
-	port, err := os.Getenv("PORT")
-	if err != nil {
-		port = 8000
-	}
+	port := os.Getenv("PORT")
 	mappings.Router.Run(port)
 
 }
